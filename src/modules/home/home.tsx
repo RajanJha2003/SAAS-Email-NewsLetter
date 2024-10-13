b5cb99add5bded7785d79af1e4f6a2afafa29fbf
Rajan <rajanj448@student.sfit.ac.in>
2024-10-13 22:50:58 +0530

    Branding section

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,12 +1,14 @@
 import Header from '@/shared/widgets/header/header'
 import React from 'react'
 import Banner from './elements/banner'
+import Branding from './elements/branding'
 
 const Home = () => {
   return (
     <div>
         <Header />
         <Banner />
+        <Branding />
     </div>
   )
 }
