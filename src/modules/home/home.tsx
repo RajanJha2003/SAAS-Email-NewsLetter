d56ff7b8804f614cf3728cfe393ad9dd4bfb136c
Rajan <rajanj448@student.sfit.ac.in>
2024-10-13 22:16:16 +0530

    assets added

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,10 +1,12 @@
 import Header from '@/shared/widgets/header/header'
 import React from 'react'
+import Banner from './elements/banner'
 
 const Home = () => {
   return (
     <div>
         <Header />
+        <Banner />
     </div>
   )
 }
